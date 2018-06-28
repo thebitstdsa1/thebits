7cda3a3cc5b63705aa49232baa31bce880722fc3
gabi <gabi>
2018-06-27 23:19:02 -0300

    initial commit

---
+++ src/test/java/presentacion/MultiplicarTest.java
@@ -0,0 +1,14 @@
+package presentacion;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MultiplicarTest {
+
+	@Test
+	void testMultiplicar() {
+		fail("Not yet implemented");
+	}
+
+}
