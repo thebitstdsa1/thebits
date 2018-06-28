0f7a7ab986b94d205c16318259fcd5ccacc86112
gabi <gabi@ligamio>
2018-06-28 09:50:25 -0300

    TB-2 init commit

--- src/test/java/presentacion/MultiplicarTest.java
+++ src/test/java/presentacion/MultiplicarTest.java
@@ -11,4 +11,17 @@ class MultiplicarTest {
 		//fail("Not yet implemented");
 	}
 	
+	@Test //debe fallar pq esta enviando 2*3 y tiene que dar 6
+	void testDebeFallar() {
+		Calculadora test = new Calculadora();
+		assertNotEquals(4, test.multiplicar(2, 3), "multiplicar(2,3) debe dar 6");
+	}
+	
+	@Test //debe pasar pq 2 por 4 es 8
+	void testDebeAndar() {
+		Calculadora test = new Calculadora();
+		assertEquals(8, test.multiplicar(2, 4), "multiplicar(2,4) debe dar 8");
+	}
+		
+	
 }
