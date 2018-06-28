a0ad3eb83e4dca835ae7cc7a84d46b15d41dbdb4
gabi <gabi>
2018-06-27 23:11:39 -0300

    initial commit

---
+++ src/main/java/presentacion/Calculadora.java
@@ -0,0 +1,9 @@
+package presentacion;
+
+public class Calculadora {
+	
+	public int multiplicar(int a, int b) {
+		return a*b;
+	}
+	
+}
---
+++ src/test/java/presentacion/Multiplicar.java
@@ -0,0 +1,14 @@
+package presentacion;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Multiplicar {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
